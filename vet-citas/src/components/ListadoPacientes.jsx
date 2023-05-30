daae761e9351d9e1a68e8d820dae9997c2fe91aa
jaquimbayoc7 <jaquimbayoc7@gmail.com>
2023-05-29 19:55:34 -0500

    actualización

--- vet-citas/src/components/ListadoPacientes.jsx
+++ vet-citas/src/components/ListadoPacientes.jsx
@@ -5,20 +5,28 @@ function ListadoPacientes({pacientes}) {
   console.log(pacientes);
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
-        <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-        <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
+      {pacientes && pacientes.length ? (
+          <>
+            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
 
-        {
-          pacientes.map(paciente =>(
-            <Pacientes 
-              paciente = {paciente}
-            />
-          ))
-        }
-        
-        
-        
+            {
+              pacientes.map(paciente =>(
+                <Pacientes 
+                  key = {paciente.id}
+                  paciente = {paciente}
+                />
+              ))
+            }
+          </>
+      ):(
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">Comienza Agregando {''}
+          <span className="text-indigo-600 font-bold">Los pacientes apareceran en este espacio</span></p>
+        </>
+      )}
     </div>
   )
 }
--- vet-citas/src/components/Pacientes.jsx
+++ vet-citas/src/components/Pacientes.jsx
@@ -17,6 +17,12 @@ function Pacientes({paciente}) {
           <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
             <span className="font-normal normal-case">{sintomas}</span>
           </p>
+          <div className="flex justify-between mt-10">
+            <button type="button" className="py-2 px-10
+             bg-indigo-600 font-bold uppercase text-white rounded-lg hover:bg-indigo-500">Editar</button>
+            <button type="button" className="py-2 px-10
+             bg-red-600 font-bold uppercase text-white rounded-lg hover:bg-red-400">Eliminar</button>
+          </div>
         </div>
   )
 }
